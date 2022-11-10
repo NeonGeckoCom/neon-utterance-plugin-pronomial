ecbab16a8ae75e5e9b67e1a9e40c8cc86ed9eb64
NeonDmitry <dmitry@neon.ai>
2022-11-10 19:16:51 +0200

    init fix

--- neon_utterance_pronomial_plugin/__init__.py
+++ neon_utterance_pronomial_plugin/__init__.py
@@ -41,21 +41,3 @@ class PronomialTransformer(UtteranceTransformer):
         solved = [u for u in solved if u not in utterances]
         # return augmented utterances
         return solved + utterances, {}
-
-test = [
-    "My neighbors have a cat. It has a bushy tail.",
-    "Here is the book now take it.",
-    "The sign was too far away for the boy to read it.",
-    "Dog is man's best friend. It is always loyal.",
-    "The girl said she would take the trash out.",
-    "I voted for Nader because he is clear about his values. His ideas represent a majority of the nation. He is better than Rajeev.",
-    "Jack von Doom is one of the top candidates in the elections. His ideas are unique compared to Neha's.",
-    "Members voted for John because they see him as a good leader.",
-    "Leaders around the world say they stand for peace.",
-    "My neighbours just adopted a puppy. They care for it like a baby.",
-    "I have many friends. They are an important part of my life.",
-    "Jarbas has a dog named Jurebes. He loves his dog!",
-    "London is the capital and most populous city of England and the United Kingdom. Standing on the River Thames in the south east of the island of Great Britain, London has been a major settlement for two millennia. It was founded by the Romans, who named it Londinium."
-]
-for utt in test:
-    print(PronomialTransformer().transform([utt])[0])
